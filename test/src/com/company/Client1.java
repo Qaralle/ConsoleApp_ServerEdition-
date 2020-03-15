64b22d76219f3532b3ffc3b465fae9d077a7f73b
Maxim Antonov <57999732+Qaralle@users.noreply.github.com>
2020-03-15 23:16:57 +0300

    Add files via upload

---
+++ test/src/com/company/Client1.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.*;
+import java.net.*;
+
+public class Client1
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader inFromUser =
+                new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName(null);
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[4*1024];
+        while (true) {
+            String sentence = inFromUser.readLine();
+            sendData = sentence.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 7877);
+            clientSocket.send(sendPacket);
+            DatagramPacket hui = new DatagramPacket(receiveData,receiveData.length);
+            clientSocket.receive(hui);
+            System.out.println(new String(receiveData));
+
+
+        }
+    }
+}
---
+++ test/src/com/company/Server.java
@@ -0,0 +1,73 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+
+public class Server
+{
+    private static final String hui = "hui";
+
+
+    public static void main(String args[]) throws Exception
+    {
+        DatagramChannel chan = DatagramChannel.open();
+        chan.socket().bind( new InetSocketAddress( 7877 ) );
+        chan.configureBlocking(false);
+
+        Selector selector = Selector.open();
+        chan.register(selector,SelectionKey.OP_READ);
+        ByteBuffer buffer = ByteBuffer.allocate(4*1024);
+
+        while (true) {
+            selector.select();
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iter = selectedKeys.iterator();
+            while (iter.hasNext()) {
+
+                SelectionKey key = iter.next();
+
+                if (key.isReadable()) {
+                    buffer.clear();
+                    buffer.put(new byte[4*1024]);
+                    buffer.clear();
+                    action(buffer, key);
+                }
+                iter.remove();
+            }
+        }
+
+    }
+
+    private static void action(ByteBuffer buffer, SelectionKey key) throws IOException, InterruptedException {
+
+        DatagramChannel channel = (DatagramChannel)key.channel();
+
+        buffer.clear();
+        System.out.println("патключаус к нон блок аналу");
+        SocketAddress from = channel.receive(buffer);
+        System.out.println("внатуре успех");
+
+        if (from != null) {
+            buffer.flip();
+            String val = new String(buffer.array());
+            System.out.println(from);
+            System.out.println(channel);
+            System.out.println("Сервер получил по ебалу: "+val);
+
+            channel.send(ByteBuffer.wrap(hui.getBytes()),from);
+        }
+
+        System.out.println( "отрубаюсь" );
+        TimeUnit.SECONDS.sleep( 5 );
+
+    }
+
+}
